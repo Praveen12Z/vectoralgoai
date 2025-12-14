c85352e362eef0c836812a00337001fc3b6267e1
Praveen12Z <64327771+Praveen12Z@users.noreply.github.com>
2025-12-14 22:01:11 +0100

    2_Dashboard.py

--- pages/2_Dashboard.py
+++ pages/2_Dashboard.py
@@ -1,8 +1,3 @@
-import streamlit as st
-from ui.shared import load_css
 from mvp_dashboard import run_mvp_dashboard
-
-st.set_page_config(page_title="Dashboard", layout="wide")
-load_css()
 
 run_mvp_dashboard()

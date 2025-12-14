5a630c99c98a29cbe9a47fdfdb00ba6efb1535e1
Praveen12Z <64327771+Praveen12Z@users.noreply.github.com>
2025-12-14 22:35:43 +0100

    app.py

--- app.py
+++ app.py
@@ -4,19 +4,6 @@ from pages_shared import load_css, top_nav
 st.set_page_config(page_title="VectorAlgoAI", layout="wide")
 
 load_css()
-
-# Top Navbar (works everywhere)
 top_nav(active="Home")
 
-st.title("🧪 VectorAlgoAI")
-
-st.markdown(
-    """
-    <div class="container">
-      <div class="notice">
-        Use the top navigation to switch pages.
-      </div>
-    </div>
-    """,
-    unsafe_allow_html=True
-)
+st.switch_page("pages/1_Home.py")

c8ce26222869459a95878518438248904eaf89e0
Praveen12Z <64327771+Praveen12Z@users.noreply.github.com>
2025-12-14 18:32:38 +0100

    1_Product.py

--- pages/1_Product.py
+++ pages/1_Product.py
@@ -1,3 +1,9 @@
+def load_css():
+    with open("assets/styles.css") as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+load_css()
+
 import streamlit as st
 from ui.product_sections import render_product_sections
 
